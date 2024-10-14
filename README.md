bc2069e61e12d784b1fe2f7ce7527ee8b5b56b85
vishaalS7 <ivishaal1411@gmail.com>
2024-10-14 17:22:09 +0530

    Defi protocol

--- README.md
+++ README.md
@@ -64,3 +64,14 @@ $ forge --help
 $ anvil --help
 $ cast --help
 ```
+
+
+
+1. Relative Stability: Achored or Pegged to the US Dollar
+   1. Chainlink Pricefeed
+   2. Function to convert ETH & BTC to USD
+2. Stability Mechanism (Minting/Burning): Algorithmicly Decentralized
+   1. Users may only mint the stablecoin with enough collateral
+3. Collateral: Exogenous (Crypto)
+   1. wETH
+   2. wBTC
---
+++ src/ArceusStableCoin.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/*
+ * @title: DecentralizedStableCoin
+ * @author: 0Glitch 'extracted from cyfrin updraft for education purpose'
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ */
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/openzeppelin-contracts/token/ERC20/extensions/ERC20Burnable.sol"; //ERC20Burnable includes `burn` functionality for our tokens which will be important when we need to take the asset out of circulation to support stability.
+import {Ownable} from "@openzeppelin/openzeppelin-contracts/access/Ownable.sol";
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) external onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
